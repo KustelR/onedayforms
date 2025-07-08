fcc84cfbebeabf21ea08d9dffc58dcc8d55048ed
KustelR <dmitriysstudios2@gmail.com>
2025-07-08 17:06:33 +0400

    feat: separating FormView into component

--- src/app/[id]/page.tsx
+++ src/app/[id]/page.tsx
@@ -1,8 +1,5 @@
-import CustomTextArea from "@/components/CustomTextArea";
-import RadioInput from "@/components/RadioInput";
-import getForm, { FormItem } from "@/lib/getForm";
-import { TextInput } from "@/components/TextInput";
-import { nanoid } from "nanoid";
+import FormView from "@/components/FormView";
+import getForm from "@/lib/getForm";
 
 export default async function Page({
   params,
@@ -13,40 +10,7 @@ export default async function Page({
   const form = await getForm(id);
   return (
     <div className="w-full h-screen flex justify-center bg-background text-foreground">
-      <div className="p-3 w-3xl bg-white space-y-3 shadow-md">
-        <div>
-          <h1 className="text-4xl text-center font-sans">{form.name}</h1>
-          <p className="px-3 font-merriweather">{form.description}</p>
-        </div>
-        <form className="space-y-3" action="/" method="post">
-          <ul className="space-y-3">
-            {form.items.map((item) => {
-              return (
-                <li key={"id" in item.props ? item.props.id : nanoid()}>
-                  {getElement(item)}
-                </li>
-              );
-            })}
-          </ul>
-          <button
-            className="bg-blue-500 shadow-md font-bold text-white rounded-xl text-2xl px-8 py-0.5 cursor-pointer hover:bg-blue-600"
-            type="submit"
-          >
-            Submit
-          </button>
-        </form>
-      </div>
+      <FormView form={form} />
     </div>
   );
-}
-
-function getElement(item: FormItem): React.JSX.Element {
-  switch (item.type) {
-    case "text":
-      return <TextInput {...item.props} />;
-    case "textarea":
-      return <CustomTextArea {...item.props} />;
-    case "radio":
-      return <RadioInput {...item.props} />;
-  }
 }
