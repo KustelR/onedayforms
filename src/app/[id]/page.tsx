bc16b682d72192d1cb2eee5e68ff3d4cd300f11f
KustelR <dmitriysstudios2@gmail.com>
2025-07-08 15:33:36 +0400

    feat: ssr page for viewing form

---
+++ src/app/[id]/page.tsx
@@ -0,0 +1,52 @@
+import CustomTextArea from "@/components/CustomTextArea";
+import RadioInput from "@/components/RadioInput";
+import getForm, { FormItem } from "@/lib/getForm";
+import { TextInput } from "@/components/TextInput";
+import { nanoid } from "nanoid";
+
+export default async function Page({
+  params,
+}: {
+  params: Promise<{ id: string }>;
+}) {
+  const { id } = await params;
+  const form = await getForm(id);
+  return (
+    <div className="w-full h-screen flex justify-center bg-background text-foreground">
+      <div className="p-3 w-3xl bg-white space-y-3 shadow-md">
+        <div>
+          <h1 className="text-4xl text-center font-sans">{form.name}</h1>
+          <p className="px-3 font-merriweather">{form.description}</p>
+        </div>
+        <form className="space-y-3" action="/" method="post">
+          <ul className="space-y-3">
+            {form.items.map((item) => {
+              return (
+                <li key={"id" in item.props ? item.props.id : nanoid()}>
+                  {getElement(item)}
+                </li>
+              );
+            })}
+          </ul>
+          <button
+            className="bg-blue-500 shadow-md font-bold text-white rounded-xl text-2xl px-8 py-0.5 cursor-pointer hover:bg-blue-600"
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </div>
+    </div>
+  );
+}
+
+function getElement(item: FormItem): React.JSX.Element {
+  switch (item.type) {
+    case "text":
+      return <TextInput {...item.props} />;
+    case "textarea":
+      return <CustomTextArea {...item.props} />;
+    case "radio":
+      return <RadioInput {...item.props} />;
+  }
+}
