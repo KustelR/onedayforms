8b69adc01f9edce5054320fe068de2836b878c96
KustelR <dmitriysstudios2@gmail.com>
2025-07-08 15:33:47 +0400

    feat: main page placeholder

---
+++ src/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+  return (
+    <div className="w-full h-screen flex justify-center bg-background text-foreground">
+      <div className="w-3xl bg-white">main page</div>
+    </div>
+  );
+}
